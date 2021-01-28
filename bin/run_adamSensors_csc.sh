874d664efc034a41babcf34c74245e0117c3afdb
Colin Winslow <cwinslow@lsst.org>
2021-01-28 16:18:48 -0700

    Gets things to a working state

---
+++ bin/run_adamSensors_csc.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import asyncio
+from lsst.ts import adamSensors
+asyncio.run(adamSensors.AdamCSC.amain(index=None))
--- python/lsst/ts/adamSensors/adamSensorsCSC.py
+++ python/lsst/ts/adamSensors/adamSensorsCSC.py
@@ -1,7 +1,9 @@
 from lsst.ts import salobj
 from lsst.ts.adamSensors.model import AdamModel
+from numpy import poly1d
 import pathlib
 import asyncio
+import concurrent
 
 
 class AdamCSC(salobj.ConfigurableCsc):
@@ -9,32 +11,41 @@ class AdamCSC(salobj.ConfigurableCsc):
     CSC for simple sensors connected to an ADAM controller
     """
 
-    def __init__(self, config_dir=None, initial_state=salobj.State.STANDBY, initial_simulation_mode=0):
-        schema_path = pathlib.Path(__file__).resolve().parents[4].joinpath("schema", "AdamSensors.yaml")
+    def __init__(
+        self, config_dir=None, initial_state=salobj.State.STANDBY, simulation_mode=0
+    ):
+        schema_path = (
+            pathlib.Path(__file__)
+            .resolve()
+            .parents[4]
+            .joinpath("schema", "AdamSensors.yaml")
+        )
         super().__init__(
             "AdamSensors",
             index=0,
             schema_path=schema_path,
             config_dir=config_dir,
             initial_state=initial_state,
-            initial_simulation_mode=initial_simulation_mode,
+            simulation_mode=simulation_mode,
         )
 
-        self.connected = False
         self.adam = None
         self.config = None
+        self.start_timeout = 10
+        self.valid_simulation_modes = [0, 1]
 
-        # setup asyncio tasks for the loop
-        done_task = asyncio.Future()
-        done_task.set_result(None)
-        self.telemetryLoopTask = done_task
+        self.telemetry_loop_task = salobj.make_done_future()
 
-    async def begin_start(self, id_data):
-        await super().begin_start(id_data)
-        self.adam = AdamModel(self.log, simulation_mode=True)
-        await self.adam.connect(self.config.adam_ip, self.config.adam_port)
-        self.connected = True
-        await self.telemetry_loop()
+    async def begin_start(self, data):
+        """
+        Sets up the model which communicates with the ADAM hardware, and
+        initiates the telemetry publishing loop
+        """
+        self.cmd_start.ack_in_progress(data, timeout=self.start_timeout)
+        await super().begin_start(data)
+        self.adam = AdamModel(self.log, simulation_mode=self.simulation_mode)
+        self.adam.connect(self.params.ip, self.params.port)
+        self.telemetry_loop_task = asyncio.create_task(self.telemetry_loop())
 
     async def telemetry_loop(self):
         """
@@ -42,16 +53,108 @@ class AdamCSC(salobj.ConfigurableCsc):
         the ADAM device, converts them into the appropriate units
         for various sensor types, and publishes them as telemetry
         """
+        # set up dictionary
+        sensors = {
+            0: (
+                self.config.analog_input_0_type,
+                poly1d(self.config.analog_input_0_coefficients),
+            ),
+            1: (
+                self.config.analog_input_1_type,
+                poly1d(self.config.analog_input_1_coefficients),
+            ),
+            2: (
+                self.config.analog_input_2_type,
+                poly1d(self.config.analog_input_2_coefficients),
+            ),
+            3: (
+                self.config.analog_input_3_type,
+                poly1d(self.config.analog_input_3_coefficients),
+            ),
+            4: (
+                self.config.analog_input_4_type,
+                poly1d(self.config.analog_input_4_coefficients),
+            ),
+            5: (
+                self.config.analog_input_5_type,
+                poly1d(self.config.analog_input_5_coefficients),
+            ),
+        }
 
-        while self.connected:
-            voltages = self.adam.read_voltage()
-            print(voltages)
-            asyncio.sleep(1)
+        # figure out which topics to publish
+        hasTemperature = False
+        hasPressure = False
+        for s in sensors:
+            if sensors[s][0] == "Pressure":
+                hasPressure = True
+            if sensors[s][0] == "Temperature":
+                hasTemperature = True
+
+        self.log.debug("hasPressure = " + str(hasPressure))
+
+        loop = asyncio.get_event_loop()
+        executor = concurrent.futures.ThreadPoolExecutor()
+
+        outputs = [0, 0, 0, 0, 0, 0]
+        self.log.debug("about to start telemetry loop")
+        while True:
+            voltages = await loop.run_in_executor(executor, self.adam.read_voltage)
+
+            # convert the voltage into whatever units, according to the
+            # polynomial defined in configuration
+            for i in range(6):
+                outputs[i] = sensors[i][1](voltages[i])
+
+            # Assemble telemetry topics
+            # Channel 0
+            if sensors[0][0] == "Pressure":
+                self.tel_pressure.set(pressure_ch0=outputs[0])
+            elif sensors[0][0] == "Temperature":
+                self.tel_temperature.set(temp_ch0=outputs[0])
+
+            # Channel 1
+            if sensors[1][0] == "Pressure":
+                self.tel_pressure.set(pressure_ch1=outputs[1])
+            elif sensors[1][0] == "Temperature":
+                self.tel_temperature.set(temp_ch1=outputs[1])
+
+            # Channel 2
+            if sensors[2][0] == "Pressure":
+                self.tel_pressure.set(pressure_ch2=outputs[2])
+            elif sensors[2][0] == "Temperature":
+                self.tel_temperature.set(temp_ch2=outputs[2])
+
+            # Channel 3
+            if sensors[3][0] == "Pressure":
+                self.tel_pressure.set(pressure_ch3=outputs[3])
+            elif sensors[3][0] == "Temperature":
+                self.tel_temperature.set(temp_ch3=outputs[3])
+
+            # Channel 4
+            if sensors[4][0] == "Pressure":
+                self.tel_pressure.set(pressure_ch4=outputs[4])
+            elif sensors[4][0] == "Temperature":
+                self.tel_temperature.set(temp_ch4=outputs[4])
+
+            # Channel 5
+            if sensors[5][0] == "Pressure":
+                self.tel_pressure.set(pressure_ch5=outputs[5])
+            elif sensors[5][0] == "Temperature":
+                self.tel_temperature.set(temp_ch5=outputs[5])
+
+            # publish telemetry
+            if hasPressure:
+                self.log.debug("publishing pressure")
+                self.tel_pressure.put()
+            if hasTemperature:
+                self.log.debug("publishing temperature")
+                self.tel_temperature.put()
+
+            await asyncio.sleep(self.heartbeat_interval)
 
     @staticmethod
     def get_config_pkg():
         return "ts_config_eas"
 
     async def configure(self, config):
-        print("csc config")
         self.config = config
