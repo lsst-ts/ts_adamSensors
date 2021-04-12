bd1bc2ac8f36dc943273c150f80e071431342e20
Colin Winslow <cwinslow@lsst.org>
2021-04-12 15:36:49 -0700

    meta.yaml

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 setup(
     name="ts-adamSensors",
     packages=["lsst.ts.adamSensors"],
-    version=1.0.0,
-    install_requires['ts_salobj', 'pymodbus'],
+    version=0.8,
+    install_requires=['ts_salobj', 'pymodbus'],
 )
